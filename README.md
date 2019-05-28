45ad90732ad1123718dd18e4a1fdac085f2ace10
Marek <pruteanupopescumarek@gmail.com>
2019-05-28 13:31:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# GraphQL Tutorial
+
+### Ruby version
+```sh
+>= 2.6.3
+```
+### System dependencies
+
+### Configuration
+
+### Database creation
+```sh
+bundle exec rails db:create
+```
+### Database initialization
+
+### How to run the test suite
+```sh
+bundle exec rails test
+```
+### Services (job queues, cache servers, search engines, etc.)
+
+### Deployment instructions
---
+++ app/graphql/mutations/create_link.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class CreateLink < BaseMutation
+    # Arguments passed to the 'resolved' method
+    argument :description, String, required: true
+    argument :url, String, required: true
+
+    # Return type from the mutation
+    type Types::LinkType
+
+    def resolve(description: nil, url: nil)
+      Link.create!(
+        description: description,
+        url: url
+      )
+    end
+  end
+end
