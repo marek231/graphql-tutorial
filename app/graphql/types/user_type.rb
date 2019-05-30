2b2547178c630f7188a48e25cff7555cc430e85d
Marek <pruteanupopescumarek@gmail.com>
2019-05-30 10:59:29 +0300

    Relate users with their votes

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,5 +5,8 @@ module Types
 
     # Just for tutorial purposes
     field :email, String, null: false
+
+    field :votes, [VoteType], null: false
+    field :links, [LinkType], null: false
   end
 end
